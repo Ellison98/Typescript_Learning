f17be8cd84c8b86761447bd34af4f5c04420ad57
imseongsu <in05013300@gmail.com>
2024-10-28 22:58:13 +0900

    interface

--- proGrammers/TypeScript_Study/app.js
+++ proGrammers/TypeScript_Study/app.js
@@ -1,4 +1,21 @@
-function logName(name) {
-    console.log(name);
+// 변수의 데이터 타입 명시
+var stdId = 1111;
+var stdName = '홍길동';
+var stdAge = 20;
+var stdGender = 'male';
+var course = "TypeScript";
+var completed = false;
+// 함수의 데이터 타입 명시 ( 매개변수와 리턴값, 함수의 리턴 타입)
+function Plus(a, b) {
+    return a + b;
 }
-logName('Seoung Su');
+function getInfo(id) {
+    return {
+        stdId: id,
+        stdName: 'IM',
+        stdGender: 'femaie',
+        course: 'javascript',
+        completed: true,
+    };
+}
+console.log(getInfo(1000));
--- proGrammers/TypeScript_Study/app.ts
+++ proGrammers/TypeScript_Study/app.ts
@@ -1,5 +1,34 @@
-function logName(name : string) {
-    console.log(name);
+// 변수의 데이터 타입 명시
+let stdId : number = 1111;
+let stdName : string = '홍길동';
+let stdAge : number = 20;
+let stdGender : string = 'male';
+let course : string = "TypeScript";
+let completed : boolean = false;
+
+
+// 함수의 데이터 타입 명시 ( 매개변수와 리턴값, 함수의 리턴 타입)
+function Plus(a : number, b : number) : number {
+    return a + b;
 }
 
-logName('Seoung Su');
+interface Student {
+    stdId : number;
+    stdName : string;
+    stdAge? : number;
+    stdGender : string;
+    course : string;
+    completed : boolean;
+}
+
+function getInfo(id : number) : Student {
+    return {
+        stdId : id,
+        stdName : 'IM',
+        stdGender : 'femaie',
+        course : 'javascript',
+        completed : true,
+    };
+}
+
+console.log(getInfo(1000));
