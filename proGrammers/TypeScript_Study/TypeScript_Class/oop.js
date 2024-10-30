32ed2db655753da473c4f6fc867a8b9c2a1c0514
imseongsu <in05013300@gmail.com>
2024-10-30 15:32:36 +0900

    class

--- proGrammers/TypeScript_Study/TypeScript_Class/oop.js
+++ proGrammers/TypeScript_Study/TypeScript_Class/oop.js
@@ -2,3 +2,8 @@
 var empName;
 var age;
 var empJob;
+function printEmp(empName, age, empJob) {
+    //  console.log(`이름 : ${empName}, 나이 : ${age}, 직업 : ${empJob}`);
+    console.log(empName + '의 나이는' + age + '이며 직업은' + empJob + '입니다.');
+}
+printEmp('홍길동', 30, '개발자');
--- proGrammers/TypeScript_Study/TypeScript_Class/oop.ts
+++ proGrammers/TypeScript_Study/TypeScript_Class/oop.ts
@@ -3,3 +3,9 @@ let empName : string;
 let age : number;
 let empJob : string;
 
+function printEmp(empName : string, age : number, empJob : string) : void {
+    //  console.log(`이름 : ${empName}, 나이 : ${age}, 직업 : ${empJob}`);
+    console.log(empName+ '의 나이는' + age + '이며 직업은' + empJob + '입니다.');
+}
+
+printEmp('홍길동', 30, '개발자');
