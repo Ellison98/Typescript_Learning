a98c9c2f808d779d3a9d602e61b62af3f0f1a847
imseongsu <in05013300@gmail.com>
2024-11-02 21:10:21 +0900

    Functional_Component

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import ClassCom from './ClassCom';
+import FuncCom from './FuncCom';
 
 function App() {
   return (
     <div className="container">
       <ClassCom></ClassCom>
+      <FuncCom></FuncCom>
     </div>
   );
 }
---
+++ todolist/src/FuncCom.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FuncCom() {
+    return (
+        <div>
+            Functional Component
+        </div>
+    );
+}
+
+export default FuncCom;
