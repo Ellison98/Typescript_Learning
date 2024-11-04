2300af087fb13d7132a49f93e53a61279f8c99a4
imseongsu <in05013300@gmail.com>
2024-11-04 20:19:08 +0900

    slices

---
+++ REACT-TASK-APP/src/store/slices/boardsSlice.ts
@@ -0,0 +1,16 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    modalActive: false,
+    boardArray: [],
+};
+
+const boardSlice = createSlice({
+    name: "boards",
+    initialState,
+    reducers: {
+
+    }
+});
+
+export const board = boardSlice.reducer;
