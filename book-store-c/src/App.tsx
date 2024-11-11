5aec5304cad1e6370cf95ac5f1df57aea2a421d4
imseongsu <in05013300@gmail.com>
2024-11-11 19:22:34 +0900

    Layout_components

--- book-store-c/src/App.tsx
+++ book-store-c/src/App.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import Home from './pages/Home';
+import Layout from './components/layout/Layout';
+import Detail from './pages/Detail';
 
 function App() {
-  return <Home />
+  // return <Layout children={<Detail />} />
+  return (
+    <Layout>
+      <Home />
+    </Layout>
+  )
 }
 
 export default App;
--- book-store-c/src/pages/Home.tsx
+++ book-store-c/src/pages/Home.tsx
@@ -1,15 +1,8 @@
-import Header from "../components/common/Header";
-import { formatNumber } from "../utils/format";
-
-const COUNT = 100000;
-
 function Home() {
     return (
-        <div>
-            <Header />
+        <>
             <div>home</div>
-            <div>count : {formatNumber(COUNT)}</div>
-        </div>
+        </>
     )
 }
 
