e1be6b0ecd06708065a6aafb1c746d768a64bf56
imseongsu <in05013300@gmail.com>
2024-11-13 15:09:25 +0900

    commit

--- book-store-c/src/App.tsx
+++ book-store-c/src/App.tsx
@@ -1,8 +1,7 @@
-import React, { useContext, useState } from 'react';
 import Home from './pages/Home';
 import Layout from './components/layout/Layout';
 import ThemeSwitcher from './components/header/ThemeSwitcher';
-import { BookStoreThemeProvider, ThemeContext } from './context/themeContext';
+import { BookStoreThemeProvider } from './context/themeContext';
 
 function App() {
   return (
