d73bb28ed63340a956dfed6b9cde8b88d52d6522
imseongsu <in05013300@gmail.com>
2025-01-28 14:13:18 +0900

    props

--- REACT_CODING_TEST/react-test/src/App.js
+++ REACT_CODING_TEST/react-test/src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 
+function Counter(props) {
+  return (
+    <div>
+      <h1>{props.title} Counter</h1>
+      <button>+</button> {props.initValue}
+    </div>
+  );
+}
+
 function App() {
   return (
     <div>
-      <h1>Counter</h1>
-      <button>+</button> 0
+      <Counter title="불면증 카운터" initValue="10"/>
     </div>
   );
 }
